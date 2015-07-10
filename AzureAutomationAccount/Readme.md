28f39b67993ca2b8514d1465c147662e8c4d5be7
Stijn Callebaut <stijnc@fooji.net>
2015-07-10 21:56:38 +0200

    AzureAutomation test

---
+++ AzureAutomationAccount/Readme.md
@@ -0,0 +1,6 @@
+﻿## this is a test
+
+1. number one item
+2. number two
+
+### header 3
---
+++ AzureAutomationRunbook/Scripts/AzureAutomationWorkflow.ps1
@@ -0,0 +1,7 @@
+#
+# AzureAutomationWorkflow.ps1
+#
+Workflow Hello-ARM {
+
+	Write-Output "Hello, I'm created using ARM :-) "
+}
